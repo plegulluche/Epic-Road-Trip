e37c8cf036a73bbe2a252016da9b6229daad6164
hnabil90 <nabil.heddak@epitech.eu>
2023-02-28 17:17:55 +0100

    cancel

--- api/controllers/skyscannerControllers.js
+++ api/controllers/skyscannerControllers.js
@@ -1,1 +1,41 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const GlobalStats = require('./../models/globalStatsModel.js');
+
+const dotenv = require('dotenv');
+dotenv.config({path:"./../config.env"});
+
+
+const SKY_KEY = process.env.SKYSCANNER_API_KEY;
+
+
+// set url as const
+const coinMarKey = process.env.COINMCAP_API_KEY;
+const urlGlobalStats = "https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest"
+
+
+getData = async (url = '', apiKey='') => {
+
+    const res = await fetch(url, {
+    method: "GET",
+    headers:{"X-CMC_PRO_API_KEY":apiKey}
+    });
+    return res.json()
+};
+
+ (async (req,res) => {
+    try {
+        const json = await getData(urlGlobalStats,coinMarKey);
+        const x = json.data
+        const y = json.data.quote.USD
+        const data = Object.assign(x,y)
+        GlobalStats.remove({}, function(err) {console.log('collection removed') });
+        GlobalStats.create(data);
+    } catch (err){
+        console.log(err)
+    }
+})();
+
+/* test */
+
+
