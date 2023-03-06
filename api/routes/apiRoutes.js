04af4f2e182a47d215847327753a956fcc8e9563
Pierre-Legulluche <pierre.le-gulluche@epitech.eu>
2023-03-06 13:52:33 +0100

    [EDIT] edited routes in apiRoutes.js, simplified the routes

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -2,37 +2,28 @@ const router = require('express').Router();
 
 // ALL BELOW ROUTE
 // *** get all categories of required events,bars,restaurants from a location, need to display top 5 or 10 of each ***
-router.get('/sumary/location', searchController.getAbstractByLocation);
+router.get('/sumary', searchController.getAbstractByLocation);
 
 // ENJOY ROUTES
 // *** get all activities from a location, need to display top 5 or 10 of each ***
-router.get('/enjoy/location', enjoycontroller.getActivitiesByLocation);
-// *** get a refined searched based on params given in the URI query string
-router.get('/enjoy/location/:params', enjoycontroller.getActivitiesByLocation);
-
-
+router.get('/enjoy', enjoycontroller.getActivitiesByLocation);
 // SLEEP ROUTES
 // *** get all hotels from a location, need to display top 5 or 10 of each ***
-router.get('/sleep/location', sleepcontroller.getSleepByLocation);
-
+router.get('/sleep', sleepcontroller.getSleepByLocation);
 // TRAVEL ROUTES
 // *** get all travel from a location, need to display top 5 or 10 of each ***
-router.get('/travel/location', travelcontroller.getTravelByLocation);
-
+router.get('/travel', travelcontroller.getTravelByLocation);
 // FOOD ROUTES
 // *** get all food from a location, need to display top 5 or 10 of each ***
-router.get('/food/location', foodcontroller.getFoodByLocation);
-
+router.get('/eat', foodcontroller.getFoodByLocation);
 // DRINK ROUTES
 // *** get all drink from a location, need to display top 5 or 10 of each ***
-router.get('/drink/location', drinkcontroller.getDrinkByLocation);
-
+router.get('/drink', drinkcontroller.getDrinkByLocation);
 // SAVE SEARCH ROUTES
 // *** save a search ***
-router.post('/search/save', searchcontroller.saveSearch);
-
+router.post('/favorites', searchcontroller.saveSearch);
 // GET SEARCH ROUTES
 // *** get all saved searches from a user ***
-router.get('search/user', searchcontroller.getSearchByUser);
+router.get('favorites/:user', searchcontroller.getUserFavorites);
 
 module.exports = router;
